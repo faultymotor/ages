5c8acdc25ab64f40cbf048c9632465f11ab8ccb4
Rishi Nath <rishi.nath@gmail.com>
2021-11-20 23:15:10 -0800

    refactor

--- index.py
+++ index.py
@@ -5,8 +5,11 @@ import matplotlib.pyplot as plt
 SIZE = 400
 POINTS = 200
 
-points = (np.random.rand(POINTS, 2) * SIZE).astype('uint8')
+def get_random_voronoi(size=SIZE, points=POINTS):
+    points = (np.random.rand(points, 2) * size).astype('uint8')
+    vor = Voronoi(points)
+    return voronoi_plot_2d(vor)
 
-vor = Voronoi(points)
-fig = voronoi_plot_2d(vor)
+fig = get_random_voronoi()
 plt.show()
+
