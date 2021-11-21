66e6230042e752c5f0b307d374ab81236537feab
Rishi Nath <rishi.nath@gmail.com>
2021-11-21 14:11:20 -0800

    pygame animation

--- ages/calc.py
+++ ages/calc.py
@@ -5,13 +5,13 @@ from ages import lloyd
 SIZE = 100
 POINTS = 500
 
-def get_random_voronoi(size=SIZE, points=POINTS):
-    points = (np.random.rand(points, 2) * size).astype('uint8')
+def get_random_voronoi(size=SIZE, num_points=POINTS):
+    points = (np.random.rand(num_points, 2) * size).astype('uint64')
     return Voronoi(points)
 
 def lloyd_relaxation(vor, reps=1, size=SIZE):
     points = lloyd.relax(vor.vertices, vor.regions)
-    points = lloyd.constrain(points, (SIZE, SIZE))
+    points = lloyd.constrain(points, (size, size))
     vor = Voronoi(points)
 
     return lloyd_relaxation(vor, reps - 1, size) if reps > 1 else vor
--- index.py
+++ index.py
@@ -1,17 +1,45 @@
-from scipy.spatial import voronoi_plot_2d
-import matplotlib.pyplot as plt
+import sys
+ 
+import pygame
+from pygame.locals import *
+
 from ages import calc
+ 
+SCREEN_SHAPE = 500
+FPS = 16.0
 
-SIZE = 100
-POINTS = 500
+vor = None
 
-vor = calc.get_random_voronoi(size=SIZE, points=POINTS)
-fig = voronoi_plot_2d(vor)
-plt.figure(1)
+def update(dt):
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
 
-vor = calc.lloyd_relaxation(vor=vor, reps=1, size=SIZE)
-fig = voronoi_plot_2d(vor)
-plt.figure(2)
+    global vor
+    if vor:
+        vor = calc.lloyd_relaxation(vor, reps=1, size=SCREEN_SHAPE)
+    else:
+        vor = calc.get_random_voronoi(size=(SCREEN_SHAPE))
 
-plt.show()
+BLACK = (0,0,0)
+BLUE = (0,204,204)
+def draw(screen):
+    screen.fill(BLACK)
+    for point in vor.points:
+        pygame.draw.circle(screen, BLUE, point, 2)
+    pygame.display.update()
+ 
+def main():
+    pygame.init()
+    fpsClock = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_SHAPE,) * 2)
 
+    dt = 1 / FPS 
+    while True: 
+        update(dt) 
+        draw(screen)
+        
+        dt = fpsClock.tick(FPS)
+
+main()
